fa82d595d9bcf98e9d8456641ffb47ff87bc7a74
MatthewSainsbury-Dale <58652976+MatthewSainsbury-Dale@users.noreply.github.com>
2021-05-19 21:15:19 +1000

    plot_order now an argument of plot_diagnostics

--- R/AllClass.R
+++ R/AllClass.R
@@ -4,10 +4,8 @@
 #' @slot diagnostics_df a \code{data.frame} containing the diagnostics for each combination of the supplied arguments
 #' @slot arg_names the argument names
 #' @slot diagnostic_names the diagnostic names
-#' @slot plot_order integer vector giving the order in which we are to assign arguments to the various aesthetics when calling \code{\link{plot_diagnostics}}
 setClass("testargs",
          representation(diagnostics_df = "data.frame",
                         arg_names = "character",
-                        diagnostic_names = "character",
-                        plot_order = "integer"))
+                        diagnostic_names = "character"))
 
--- R/plot_diagnostics.R
+++ R/plot_diagnostics.R
@@ -6,12 +6,7 @@
   return(reshape2::melt(df, id = arg_names, variable.name = "Diagnostic"))
 }
 
-# ## Order columns of a data frame based on data type.
-# ## Credit to: https://stackoverflow.com/a/50936293
-# order_cols <- function(df, col.order){
-#   df %>%
-#     select(sapply(., class) %>% .[order(match(., col.order))] %>% names)
-# }
+
 
 #' Visualise diagnostics across the tested arguments.
 #'
@@ -27,6 +22,7 @@
 #' @param object an object of class \code{testargs} from a call to \code{test_arguments()}
 #' @param focused_args the arguments we wish to plot. If \code{NULL} (default), all arguments are plotted
 #' @param average_out_non_focused_args logical indicating whether we should average over the non-focused arguments
+#' @param plot_order integer vector giving the order in which we are to assign arguments to the various aesthetics. If \code{plot_order = NULL} (default), the arguments are assigned based on their type (in the order 'numeric', 'integer', 'factor', 'character', 'logical'); otherwise, if \code{focused_args = NULL}, \code{plot_order} should be the same length as \code{object@arg_names}, and if \code{focused_args} is not \code{NULL}, \code{plot_order} should be the same length as \code{focused_args}
 #' @return a \code{ggplot} object
 #' @seealso \code{\link{test_arguments}}
 #' @export
@@ -35,7 +31,8 @@
 #' @export
 setGeneric("plot_diagnostics", function(object,
                                         focused_args,
-                                        average_out_non_focused_args)
+                                        average_out_non_focused_args,
+                                        plot_order)
   standardGeneric("plot_diagnostics"))
 
 #' @rdname plot_diagnostics
@@ -43,20 +40,28 @@ setGeneric("plot_diagnostics", function(object,
 #' @import magrittr
 #' @import stats
 #' @import methods
-# #' @importFrom dplyr select # This is for order_cols()
+#' @importFrom dplyr select
 setMethod("plot_diagnostics", signature(object = "testargs"),
           function(object,
                    focused_args = NULL,
-                   average_out_non_focused_args = TRUE) {
+                   average_out_non_focused_args = TRUE,
+                   plot_order = NULL) {
 
   if (!is(object, "testargs"))
     stop("object should be of class 'testargs'")
 
   if(is.null(focused_args)) {
     focused_args <- object@arg_names
+    if (!is.null(plot_order) && (length(plot_order) != length(object@arg_names))) {
+      stop("plot_order should have the same length as object@arg_names")
+    }
   } else {
     if (!all(focused_args %in% object@arg_names))
       stop("Some focused_args are not in the original argument names")
+
+    if (!is.null(plot_order) && (length(plot_order) != length(focused_args))) {
+      stop("plot_order should have the same length as focused_args")
+    }
   }
 
   long_df <- .long_diagnostic_df(object@diagnostics_df, object@arg_names)
@@ -84,22 +89,24 @@ setMethod("plot_diagnostics", signature(object = "testargs"),
   ## Basic plot
   g <- ggplot(long_df, aes_string(y = "value"))
 
-  # ## If we have a mixture of numeric and character/factor arguments, it would be
-  # ## best to use the numeric argument for the x-axis.
-  # ## Our strategy is to sort the data frame columns in terms of data type, and
-  # ## and then use the sorted order for focused_args.
-  # my.order <- c('numeric', 'integer', 'factor', 'character', 'logical')
-  # long_df <- long_df %>% order_cols(my.order)
-  # idx <- which(names(long_df) %in% focused_args)
-  # focused_args <- names(long_df)[idx]
-
   ## Define the plotting order of the focused arguments
-  tmp <- c()
-  for (i in object@arg_names[object@plot_order]) {
-    if (i %in% focused_args)
-      tmp <- c(tmp, i)
+  if (is.null(plot_order)) {
+    ## If we have a mixture of numeric and character/factor arguments, it would be
+    ## best to use the numeric argument for the x-axis.
+    ## Our strategy is to sort the data frame columns in terms of data type, and
+    ## and then use the sorted order for focused_args.
+    my_order <- c('numeric', 'integer', 'factor', 'character', 'logical')
+    long_df <- long_df %>% order_cols(my_order)
+    idx <- which(names(long_df) %in% focused_args)
+    focused_args <- names(long_df)[idx]
+  } else {
+    tmp <- c()
+    for (i in object@arg_names[plot_order]) {
+      if (i %in% focused_args)
+        tmp <- c(tmp, i)
+    }
+    focused_args <- tmp
   }
-  focused_args <- tmp
 
   ## Add the aesthetics
   if (length(focused_args) >= 1) {
@@ -155,3 +162,14 @@ setMethod("plot_diagnostics", signature(object = "testargs"),
 
   return(g)
 })
+
+## Order columns of a data frame based on data type.
+## Credit to: https://stackoverflow.com/a/50936293
+globalVariables(c("."))
+order_cols <- function(df, col_order){
+  # df %>%
+  #   select(sapply(., class) %>% .[order(match(., col_order))] %>% names)
+
+  df %>%
+    select(sapply(., class) %>% .[order(match(., col_order))] %>% names)
+}
